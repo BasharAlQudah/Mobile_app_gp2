4786cfbe24142bdcf1d728df738d6681024b8799
BasharAlQudah <bas20190974@std.psut.edu.jo>
2023-07-08 17:19:31 +0300

    commit blue

--- bashar_personal_app/lib/main.dart
+++ bashar_personal_app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.blue,
         body: Container(),
       ),
     );
