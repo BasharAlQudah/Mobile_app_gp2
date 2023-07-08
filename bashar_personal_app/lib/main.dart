b651753911f3e2c87aa1f0d3c81afc2e90e4e066
BasharAlQudah <bas20190974@std.psut.edu.jo>
2023-07-08 17:07:32 +0300

    commit test

--- bashar_personal_app/lib/main.dart
+++ bashar_personal_app/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.grey,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
