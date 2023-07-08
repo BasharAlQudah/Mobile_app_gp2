bf6a56e210504dc2cfe5dd6e6a9aa4e994fc9831
BasharAlQudah <bas20190974@std.psut.edu.jo>
2023-07-08 17:22:13 +0300

    commit teal

--- bashar_personal_app/lib/main.dart
+++ bashar_personal_app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: Container(),
       ),
     );
