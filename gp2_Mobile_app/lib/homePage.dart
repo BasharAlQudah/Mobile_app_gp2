06c65c7db4e29b3946d16e767bff18d28f29ba5b
BasharAlQudah <bas20190974@std.psut.edu.jo>
2023-07-30 16:59:17 +0300

    added upload image

---
+++ gp2_Mobile_app/lib/homePage.dart
@@ -0,0 +1,165 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _image;
+  final picker = ImagePicker();
+  final TextEditingController _textEditingController = TextEditingController();
+
+  Future getImage(ImageSource source) async {
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _uploadImageToFirebase() async {
+    if (_image == null) return;
+
+    try {
+      String fileName =
+          DateTime.now().millisecondsSinceEpoch.toString() + ".jpg";
+      firebase_storage.Reference ref =
+          firebase_storage.FirebaseStorage.instance.ref().child(fileName);
+
+      firebase_storage.UploadTask uploadTask = ref.putFile(_image!);
+      firebase_storage.TaskSnapshot taskSnapshot =
+          await uploadTask.whenComplete(() => null);
+
+      String imageUrl = await taskSnapshot.ref.getDownloadURL();
+      String description = _textEditingController.text;
+
+      await saveImageToFirestore(imageUrl, description);
+
+      _textEditingController.clear();
+      setState(() {
+        _image = null;
+      });
+
+      print("Image uploaded. URL: $imageUrl");
+      print("Description: $description");
+    } catch (e) {
+      print("Error uploading image: $e");
+    }
+  }
+
+  Future<void> saveImageToFirestore(String imageUrl, String description) async {
+    try {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      await firestore.collection('images').add({
+        'imageUrl': imageUrl,
+        'description': description,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      print("Image URL and description saved to Firestore.");
+    } catch (e) {
+      print("Error saving image URL and description: $e");
+    }
+    setState(() {
+      _image = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('images/login.png'), fit: BoxFit.cover),
+      ),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _image != null
+                        ? CircleAvatar(
+                            radius:
+                                100, // Change the radius to adjust the size of the circle
+                            backgroundImage: FileImage(_image!),
+                          )
+                        : const SizedBox(
+                            height: 200,
+                            width: 200,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text("Choose an option"),
+                            actions: <Widget>[
+                              TextButton(
+                                child: const Text("Upload from gallery"),
+                                onPressed: () {
+                                  getImage(ImageSource.gallery);
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              TextButton(
+                                child: const Text("Take a photo"),
+                                onPressed: () {
+                                  getImage(ImageSource.camera);
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: const Expanded(child: Text("Select Image")),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        focusNode: null,
+                        autofocus: false,
+                        controller: _textEditingController,
+                        decoration: const InputDecoration(
+                          labelText: "Disease Description ",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        Navigator.of(context).pop();
+                        await _uploadImageToFirebase();
+                      },
+                      child: const Expanded(
+                        child: Text('Upload image'),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ gp2_Mobile_app/lib/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:gp2_mobile_app/auth/auth.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  _MyLoginState createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  AuthMethods authMethods = AuthMethods();
+  Future<void> signInUser() async {
+    await authMethods.loginUser(
+      context: context,
+      email: email.text,
+      password: password.text,
+    );
+  }
+
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('images/login.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              padding: const EdgeInsets.only(left: 35, top: 80, right: 35),
+              child: const Column(
+                children: [
+                  CircleAvatar(
+                    radius: 100,
+                    backgroundImage: AssetImage('images/logo.png'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              key: _formKey,
+              child: Container(
+                padding: EdgeInsets.only(
+                    right: 35,
+                    left: 35,
+                    top: MediaQuery.of(context).size.height * 0.5),
+                child: Column(children: [
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}')
+                              .hasMatch(value)) {
+                        return 'Invalid email';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade100,
+                      filled: true,
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 6) {
+                        return "At least 6 characters";
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade100,
+                      filled: true,
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Color(0xff4c505b),
+                          fontSize: 27,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: const Color(0xff4c505b),
+                        child: IconButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              signInUser().then((value) =>
+                                  Navigator.pushNamed(context, 'homepage'));
+                            }
+                          },
+                          icon: const Icon(Icons.arrow_forward),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'register');
+                          },
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 18,
+                              color: Color(0xff4c505b),
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Forgot Password',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 18,
+                              color: Color(0xff4c505b),
+                            ),
+                          ),
+                        ),
+                      ]),
+                ]),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
